923d660710366418f8e2fe52309ab49672a93ad6
B4ckBOne <vz1@gmx.net>
2020-12-22 19:30:11 +0100

    B4ckBOne: Fixes Tilesets

--- CustomTile/Expansion-Pipoya.tsx
+++ CustomTile/Expansion-Pipoya.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.12.18" name="Expansion-Pipoya" tilewidth="32" tileheight="32" tilecount="1995" columns="15">
- <image source="../example/cija_32x32_expansionPipoya_CC0.png" width="480" height="4256"/>
+<tileset version="1.4" tiledversion="2020.12.18" name="Expansion-Pipoya" tilewidth="32" tileheight="32" tilecount="2394" columns="18">
+ <image source="cija_32x32_expansionPipoya_CC0.png" width="576" height="4256"/>
 </tileset>
