f9e3440d724ede84315018e8ed8214eff47e095d
B4ckBOne <vz1@gmx.net>
2020-12-22 21:09:47 +0100

    B4ckBOne: Fixed Pipoya Tileset

--- CustomTile/Expansion-Pipoya.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.12.18" name="Expansion-Pipoya" tilewidth="32" tileheight="32" tilecount="2394" columns="18">
- <image source="cija_32x32_expansionPipoya_CC0.png" width="576" height="4256"/>
-</tileset>
