40087b1f0a40caca3746c327298e822f279cbd69
Gigo <gigo@muc.ccc.de>
2020-12-27 19:31:20 +0100

    we dont want no tsx!

--- fussabstreifer B.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="fussabstreifer B" tilewidth="32" tileheight="32" tilecount="108" columns="9">
- <image source="tiles_png/mila/fussabstreifer B.png" width="288" height="384"/>
-</tileset>
--- main.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="main" tilewidth="32" tileheight="32" tilecount="1298" columns="22">
- <image source="tiles_png/avanti/main.png" width="718" height="1916"/>
-</tileset>
