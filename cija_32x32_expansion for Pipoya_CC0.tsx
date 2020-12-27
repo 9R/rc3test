91c6c416ef9d647c6994f18e8b00de2d304ec312
Gigo <gigo@muc.ccc.de>
2020-12-27 19:30:33 +0100

    debug rattennested images

--- cija_32x32_expansion for Pipoya_CC0.tsx
+++ cija_32x32_expansion for Pipoya_CC0.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="cija_32x32_expansion for Pipoya_CC0" tilewidth="32" tileheight="32" tilecount="1995" columns="15">
- <image source="../../../Desktop/rc3test-master/tiles_png/community/bytewerk/cija_32x32_expansion for Pipoya_CC0.png" width="480" height="4256"/>
+<tileset version="1.4" tiledversion="1.4.3" name="cija_32x32_expansion for Pipoya_CC0" tilewidth="32" tileheight="32" tilecount="2394" columns="18">
+ <image source="tiles_png/cija_32x32_expansionPipoya_CC0.png" width="576" height="4256"/>
 </tileset>
